83ecded2a5d3354b5f5603ba340c7f7d157f4a22
adityasangani <ays12@iitbbs.ac.in>
2023-10-02 13:26:11 +0530

    client

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return <Navbar />;
+};
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import Button from "../Button/Button";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-[68px] bg-[#f6f9fc]">
+      <div className="flex items-center py-3 px-4 w-[1080px]">
+        <div className="p-1 font-xl">logo</div>
+        <div className="flex items-center justify-between flex-1">
+          <div className="flex justify-center items-center list-none  ">
+            <ul className=" flex justify-center items-center">
+              <li className="px-5 py-2.5 ml-4 hover:text-[#0a2540]/75 cursor-pointer font-bold">
+                Contact
+              </li>
+              <li className="px-5 py-2.5 font-bold hover:text-[#0a2540]/75 cursor-pointer">
+                About Us
+              </li>
+              <li className="px-5 py-2.5 font-bold hover:text-[#0a2540]/75 cursor-pointer">
+                Resources
+              </li>
+              <li className="px-5 py-2.5 font-bold hover:text-[#0a2540]/75 cursor-pointer">
+                Pricing
+              </li>
+              <li className="px-5 py-2.5 font-bold hover:text-[#0a2540]/75 cursor-pointer">
+                Help
+              </li>
+            </ul>
+          </div>
+          <div className="flex justify-end items-center">
+            <Button text="Sign in" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

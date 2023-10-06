40d097eabd97777cc7bb0a57a1c5cb0bd3c018b3
agarwalrahul-22 <ag.rahul223@gmail.com>
2023-10-06 15:36:28 +0530

    added Email feature

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.use("/addUser",addUserRouter);
 const loginRouter = require("./routes/loginUser.js");
 app.use("/loginUser",loginRouter);
 
+const emailRouter = require("./routes/email.js");
+app.use("/sendEmail",emailRouter);
+
 app.listen(8000);

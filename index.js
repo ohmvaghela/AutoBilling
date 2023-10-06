2f9afe75cf46717ed9ffaa95ea6a03b53ecd952b
Aadesh Nikhade <an30@iitbbs.ac.in>
2023-10-06 15:22:21 +0530

    billSchema added1

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const addUserRouter = require("./routes/addUser.js");
 app.use("/addUser",addUserRouter);
 
+const addBillRouter = require("./routes/addBill.js");
+app.use("/addBill",addBillRouter);
+
 const loginRouter = require("./routes/loginUser.js");
 app.use("/loginUser",loginRouter);
 

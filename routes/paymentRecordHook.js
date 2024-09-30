1697318a87dfcb591d9d2a4f34af00daeb022735
vaghelaohm <vaghelaohm@gmail.com>
2024-09-30 15:03:28 +0530

    mongodb bill status update

--- routes/paymentRecordHook.js
+++ routes/paymentRecordHook.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { KEY_ID, KEY_SECRET } = process.env;
 router.use(express.json());
 const crypto = require('crypto');
+const billSchema = require("../model/billSchema");
 
 const logRazorPayment = async (req,res) => {
   const secret = KEY_SECRET;
@@ -11,10 +12,19 @@ const logRazorPayment = async (req,res) => {
   const digest = shasum.digest('hex');
 
   if (digest === req.headers['x-razorpay-signature']) {
-      console.log('Webhook verified');
-      console.log(req.body);
-      console.log(req.body.payload.payment.entity);
-
+      // console.log('Webhook verified');
+      // console.log(req.body);
+      // console.log(req.body.payload.payment.entity);
+      const bill = findBillByOrderId(req.body.payload.payment.entity.order_id);
+      if(bill){
+        bill.status = true;
+        await bill.save();
+        console.log('Bill status updated to true:', bill);
+      }
+      else{
+        console.log('Bill not found:', bill);
+        res.status(400).send('bill not found');
+      }
       res.status(200).send('Webhook received');
   } else {
       console.log('Webhook verification failed');
@@ -22,6 +32,15 @@ const logRazorPayment = async (req,res) => {
   }
 };
 
+const findBillByOrderId = async (orderId) =>{
+  const bill = await billSchema.find({orderId:orderId});
+  if(bill){
+    return bill;
+  }
+  else{
+    return NULL;
+  }
+}
 
 router.post("/",logRazorPayment);
 
